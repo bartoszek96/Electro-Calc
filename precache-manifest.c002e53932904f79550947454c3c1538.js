74e57201e32782a3379b23b4ce4c4cc4502eeaa4
Bartek <shox12.47@gmail.com>
2020-08-08 17:48:14 +0200

    Updates

---
+++ precache-manifest.c002e53932904f79550947454c3c1538.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d469c192e2e87138aaa274c71cea94",
+    "url": "/Electro-Calc/index.html"
+  },
+  {
+    "revision": "590ac3b97f13d6df5fab",
+    "url": "/Electro-Calc/static/js/2.800369a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Electro-Calc/static/js/2.800369a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6fe2fbbc391858a7333",
+    "url": "/Electro-Calc/static/js/main.63d661e3.chunk.js"
+  },
+  {
+    "revision": "2c1f9b2cca0ab7aa9fad",
+    "url": "/Electro-Calc/static/js/runtime-main.7751018b.js"
+  },
+  {
+    "revision": "4158f440a2fb9fbd0bbc79cf5da2264b",
+    "url": "/Electro-Calc/static/media/LedResistor.4158f440.jpg"
+  },
+  {
+    "revision": "852befe2ecdfcdb2618c68ae3b4f6db4",
+    "url": "/Electro-Calc/static/media/LedResistor.852befe2.jpg"
+  },
+  {
+    "revision": "40cfa2b253381ef5f9f5a23e77e1d61d",
+    "url": "/Electro-Calc/static/media/PCB.40cfa2b2.jpg"
+  },
+  {
+    "revision": "374afeeefc39a9d77121def2da37b8c3",
+    "url": "/Electro-Calc/static/media/Resistor.374afeee.jpg"
+  },
+  {
+    "revision": "f97f38b1ed64bb9918478c976af0154a",
+    "url": "/Electro-Calc/static/media/ResistorCurrent.f97f38b1.jpg"
+  },
+  {
+    "revision": "83d439a206e9ce4688de4db783fcd59b",
+    "url": "/Electro-Calc/static/media/ResistorPower.83d439a2.jpg"
+  },
+  {
+    "revision": "01f28eb95056651ca53810e8c4accfeb",
+    "url": "/Electro-Calc/static/media/ResistorResistance.01f28eb9.jpg"
+  },
+  {
+    "revision": "5294ced814659b8960e36c038cf2585b",
+    "url": "/Electro-Calc/static/media/ThreePhase.5294ced8.jpg"
+  },
+  {
+    "revision": "adefb9d7d9362acd5f651953a28fc98b",
+    "url": "/Electro-Calc/static/media/ThreePhaseCurrent.adefb9d7.jpg"
+  },
+  {
+    "revision": "395e1e28a6058c15e8ab1fd994a95d44",
+    "url": "/Electro-Calc/static/media/ThreePhasePower.395e1e28.jpg"
+  },
+  {
+    "revision": "7d5834692f45653d130cf44e4bb9b648",
+    "url": "/Electro-Calc/static/media/VoltageDivider.7d583469.jpg"
+  },
+  {
+    "revision": "dacc5a9ebfa6e6d78dd5c9ed4a3cb7a6",
+    "url": "/Electro-Calc/static/media/VoltageDivider.dacc5a9e.jpg"
+  }
+]);
